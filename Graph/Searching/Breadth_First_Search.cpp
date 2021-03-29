fbf3a9c58dc3c15f852f4418dccd654cbd0833fd
Harshit-Ramolia <ramolia.mh@iitgn.ac.in>
2021-03-29 23:27:33 +0530

    BFS

--- Graph/Searching/Breadth_First_Search.cpp
+++ Graph/Searching/Breadth_First_Search.cpp
@@ -4,7 +4,8 @@ using namespace std;
 #define ll                  long long
 #define fo(i,n)             for(ll i=0;i<n;i++)
 #define fo_vector(i,G)      for (auto& i : G)
-
+#define vi                  vector<ll>
+#define pb                  push_back
 
 #define N 10
 vector<ll> Graph[N];
@@ -42,14 +43,38 @@ void print_adjacency_list(vector<ll> Graph[]){
 
 }
 
+void Breadth_First_Search(ll root, vi Graph[]){
+
+    bool visited[N] = {false};
+    list<ll> queue;
+    queue.pb(root);
+    visited[root] = true;
+
+    while (queue.size()>0){
+        ll x = queue.front();
+        cout<<x<<" ";
+        queue.pop_front();
+
+        fo_vector(i,Graph[x]){
+            if(!visited[i]){
+                queue.push_back(i);
+                visited[i]=true;
+            }
+        }
+
+    }
+
+
+}
+
 int main(){
 
     reset_adjacency_list(Graph);
     addEdge_bidirectional(Graph,0,1);
-    addEdge_bidirectional(Graph,3,1);
-    addEdge_bidirectional(Graph,4,7);
-    addEdge_bidirectional(Graph,4,3);
-    addEdge_bidirectional(Graph,2,1);
-    
+    addEdge_bidirectional(Graph,0,2);
+    addEdge_bidirectional(Graph,1,2);
+    addEdge_bidirectional(Graph,2,3);
+    addEdge_bidirectional(Graph,3,3);
+    Breadth_First_Search(2, Graph);
     return 0;
 }
