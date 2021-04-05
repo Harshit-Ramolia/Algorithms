887979a50130af62cc1de1d173b9cf4eef222f7b
Harshit-Ramolia <ramolia.mh@iitgn.ac.in>
2021-04-05 23:27:05 +0530

    template

---
+++ Template.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+#define ll long long
+#define f(i,a,b,sign) for(ll i=a; i*sign<b*sign; i+=sign)
+
+#define ff first
+#define ss second
+
+#define vi vector <int>
+#define vl vector <ll>
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define fill(a,b) memset(a,b,sizeof(a))
+#define INF 1000000000000000000
+
+#define PI 3.1415926535
+#define mod1 1000000007 
+#define mod2 998244353
+
+#define debug1(x) "x = "<<x
+#define debug2(x,y) "x = "<<x<<" y = "<<y
+#define debug3(x,y,z) "x = "<<x<<" y = "<<y<<" z = "<<z
+
+
+int main(){
+    IOS
+    return 0;
+}
